0844d3c19c2b1b63bf43aa8724d61a1a90e54d7c
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-05-04 03:08:20 +0300

    Routes switch init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
+
+import { Navbar, Homepage, News, Exchanges, CryptoDetails, Cryptocurrencies } from './components'
+import './App.css'
 
 const App = () => {
     return (
-        <div>
-            <h1>Cryptoverse</h1>
+        <div className='app'>
+            <div className='navbar'>
+                <Navbar />
+            </div>
+            <div className='main'>
+                <Layout>
+                    <div className='routes'>
+                        <Switch>
+                            <Route exact path="/">
+                                <Homepage/>
+                            </Route>
+                            <Route exact path="/exchanges">
+                                <Exchanges/>
+                            </Route>
+                            <Route exact path="/cryptocurrencies">
+                                <Cryptocurrencies/>
+                            </Route>
+                            <Route exact path="/crypto/:coinId">
+                                <CryptoDetails/>
+                            </Route>
+                            <Route exact path="/news">
+                                <News/>
+                            </Route>
+                        </Switch>
+                    </div>
+                </Layout>
+            </div>
+            <div className='footer'></div>
         </div>
     )
 }
