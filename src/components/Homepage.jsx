b2b7c35b4e7e89d6fd6ccd6f8c92d9097f5fb1bc
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-05-05 03:12:08 +0300

    useGetCryptos hook added

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,19 +3,35 @@ import miollify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
 
+import { useGetCryptosQuery } from "../services/cryptoApi";
+
 const { Title } = Typography;
+
 const Homepage = () => {
+  const { data, isFetching } = useGetCryptosQuery();
+
+  console.log(data);
   return (
     <>
       <Title level={2} className="heading">
         Global Crypto Stats
       </Title>
       <Row>
-          <Col span={12}><Statistic title="Total Cryptocurrencies" value="5"/></Col>
-          <Col span={12}><Statistic title="Total Exchanges" value="5"/></Col>
-          <Col span={12}><Statistic title="Total Marketcap" value="5"/></Col>
-          <Col span={12}><Statistic title="Total Markets" value="5"/></Col>
-          <Col span={12}><Statistic title="Total 24h Volume" value="5"/></Col>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Exchanges" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Marketcap" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Markets" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total 24h Volume" value="5" />
+        </Col>
       </Row>
     </>
   );
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,11 +1,11 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaders = {
     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
     'X-RapidAPI-Key': 'e70a920ce9msh998a5fabf7f1588p18384fjsnb3ba3ba04241'
 }
 
-const baseUrl = 'https://coinranking1.p.rapidapi.com/exchanges'
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
 
@@ -13,11 +13,16 @@ export const cryptoApi = createApi({
     reducerPath: 'cryptoApi',
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
-        getCrypts: builder.query({
-            query: () => createRequest('/exchanges')
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
         })
     })
 })
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
+
 // const options = {
 //     method: 'GET',
 //     url: 'https://coinranking1.p.rapidapi.com/exchanges',
