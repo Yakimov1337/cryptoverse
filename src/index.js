df479e0e7163846c7e2df00b31c9070382251215
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-05-05 03:11:48 +0300

    React v18 createRoot

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react'
-import ReactDom from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import App from './App'
 import store from './app/store'
+
 import 'antd/dist/antd.css'
 
-ReactDom.render(
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
     <Router>
         <Provider store={store}>
             <App />
         </Provider>
     </Router>
-    , document.getElementById('root')
 )
