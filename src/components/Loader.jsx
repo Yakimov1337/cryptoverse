80fdca1367097990bafbf10415c47b98175edcb4
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-05-20 16:35:09 +0300

    Loader component creation

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { Spin } from 'antd';
 
-export default function Loader() {
-  return (
-    <div>Loader</div>
-  )
-}
+const Loader = () => (
+  <div className="loader">
+    <Spin />
+  </div>
+);
+
+export default Loader;
