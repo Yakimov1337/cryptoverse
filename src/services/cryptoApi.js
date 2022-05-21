2c703fdd02d0311a3ec99bd848559d4cb6a488bc
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-05-21 11:03:05 +0300

    useGetExchangesQuery hook added

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -22,11 +22,15 @@ export const cryptoApi = createApi({
         getCryptoHistory: builder.query({
             query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
         }),
+        getExchanges: builder.query({
+            query: () => createRequest('/exchanges'),
+          }),
     })
 })
 
 export const {
     useGetCryptosQuery,
     useGetCryptoDetailsQuery,
-    useGetCryptoHistoryQuery
+    useGetCryptoHistoryQuery,
+    useGetExchangesQuery
 } = cryptoApi;
